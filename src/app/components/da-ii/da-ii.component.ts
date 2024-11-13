b15582026f83f9df7bc4167aa75ec201ace6ffc3
NoemiP94 <m_emi94@hotmail.it>
2024-11-13 11:48:30 +0100

    add other components data

--- src/app/components/da-ii/da-ii.component.ts
+++ src/app/components/da-ii/da-ii.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-da-ii',
   templateUrl: './da-ii.component.html',
-  styleUrl: './da-ii.component.css'
+  styleUrl: './da-ii.component.css',
 })
-export class DaIIComponent {
-
+export class DaIIComponent implements OnInit {
+  characterData: any[] = [];
+  game_id: number = 2;
+  constructor(private games: GameService) {}
+  ngOnInit(): void {
+    this.games.getCharacters(this.game_id).subscribe((data: any) => {
+      this.characterData = data;
+      console.log(data);
+    });
+  }
 }
--- src/app/components/inquisition/inquisition.component.ts
+++ src/app/components/inquisition/inquisition.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-inquisition',
   templateUrl: './inquisition.component.html',
-  styleUrl: './inquisition.component.css'
+  styleUrl: './inquisition.component.css',
 })
-export class InquisitionComponent {
+export class InquisitionComponent implements OnInit {
+  characterData: any[] = [];
+  game_id: number = 3;
+  constructor(private games: GameService) {}
 
+  ngOnInit(): void {
+    this.games.getCharacters(this.game_id).subscribe((data: any) => {
+      this.characterData = data;
+      console.log(data);
+    });
+  }
 }
