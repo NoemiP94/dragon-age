f42a1e15c096ace7c568c4afcf6e3014620f4d31
NoemiP94 <m_emi94@hotmail.it>
2024-11-20 10:49:36 +0100

    try to display table data

--- src/app/components/origins/origins.component.ts
+++ src/app/components/origins/origins.component.ts
@@ -3,24 +3,24 @@ import { GameService } from '../../services/game.service';
 import { firstValueFrom } from 'rxjs';
 import { Router } from '@angular/router';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-}
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+// }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079 },
-  { position: 2, name: 'Helium', weight: 4.0026 },
-  { position: 3, name: 'Lithium', weight: 6.941 },
-  { position: 4, name: 'Beryllium', weight: 9.0122 },
-  { position: 5, name: 'Boron', weight: 10.811 },
-  { position: 6, name: 'Carbon', weight: 12.0107 },
-  { position: 7, name: 'Nitrogen', weight: 14.0067 },
-  { position: 8, name: 'Oxygen', weight: 15.9994 },
-  { position: 9, name: 'Fluorine', weight: 18.9984 },
-  { position: 10, name: 'Neon', weight: 20.1797 },
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   { position: 1, name: 'Hydrogen', weight: 1.0079 },
+//   { position: 2, name: 'Helium', weight: 4.0026 },
+//   { position: 3, name: 'Lithium', weight: 6.941 },
+//   { position: 4, name: 'Beryllium', weight: 9.0122 },
+//   { position: 5, name: 'Boron', weight: 10.811 },
+//   { position: 6, name: 'Carbon', weight: 12.0107 },
+//   { position: 7, name: 'Nitrogen', weight: 14.0067 },
+//   { position: 8, name: 'Oxygen', weight: 15.9994 },
+//   { position: 9, name: 'Fluorine', weight: 18.9984 },
+//   { position: 10, name: 'Neon', weight: 20.1797 },
+// ];
 
 @Component({
   selector: 'app-origins',
@@ -30,8 +30,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class OriginsComponent implements OnInit {
   characterData: any[] = [];
   gameData: any;
-  displayedColumns: string[] = ['position', 'name', 'weight'];
-  dataSource = ELEMENT_DATA;
+  gameDataArray: any[] = [];
+  displayedColumns: string[] = ['data', 'pc', 'mac'];
 
   constructor(private games: GameService, private router: Router) {}
   ngOnInit(): void {
@@ -59,8 +59,10 @@ export class OriginsComponent implements OnInit {
 
   async getGameDetail(game_id: number): Promise<void> {
     try {
-      this.gameData = await firstValueFrom(this.games.getGameData(game_id));
-      // console.log(this.gameData);
+      const gameDetail = await firstValueFrom(this.games.getGameData(game_id));
+      this.gameData = gameDetail;
+      this.gameDataArray = [gameDetail];
+      console.log(this.gameDataArray);
     } catch (error) {
       console.error('Error in the request', error);
     }
