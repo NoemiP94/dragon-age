58dd246d08fe13d638de4a57bd1d13d09512fc23
Noemi Pusceddu <132133236+NoemiP94@users.noreply.github.com>
2024-11-29 10:02:42 +0100

    add first table

--- src/app/components/goals/goals.component.ts
+++ src/app/components/goals/goals.component.ts
@@ -9,6 +9,26 @@ import { GameService } from '../../services/game.service';
 import { firstValueFrom } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
+
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.component.html',
@@ -20,6 +40,15 @@ export class GoalsComponent implements OnInit {
   goalData: any;
   dlcData: any;
   goalDlcData: { [key: number]: any[] } = {};
+
+  displayedColumns: string[] = [
+    'name',
+    'requirements',
+    'points',
+    'award_type',
+    'image_base64',
+  ];
+  // dataSource = ELEMENT_DATA;
 
   constructor(private games: GameService, private route: ActivatedRoute) {}
 
