fef72f81344b1bc02c4a0f9b1ad746a1687a144b
Noemi Pusceddu <132133236+NoemiP94@users.noreply.github.com>
2024-12-02 11:37:11 +0100

    fix icon

--- src/app/components/notfound/notfound.component.ts
+++ src/app/components/notfound/notfound.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notfound',
   templateUrl: './notfound.component.html',
-  styleUrl: './notfound.component.css'
+  styleUrl: './notfound.component.css',
 })
 export class NotfoundComponent {
-
+  constructor(private router: Router) {}
+  goToHome() {
+    this.router.navigate(['/welcome']);
+  }
 }
